6142d1992235232fba63bb6105e4f23da8bf863e
Austin Bourdier <austinbourdier@gmail.com>
2014-08-01 15:34:33 -0700

    adding saferoute folder once again

---
+++ Safe_Route/javascripts/UsersWidget/usersView.js
@@ -0,0 +1,13 @@
+SafeRoute.UsersView = {
+    render: function(results) {
+        $('input').eq(0).val(results[0].formatted_address);
+    },
+    failure: function() {
+        alert("Sorry! I Couldn't find your current location.")
+    },
+    listen: function(controller){
+     $('#contact').on('submit', controller.collectEmail.bind(this));
+  }
+}
+
+
