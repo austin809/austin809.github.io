a0705e04772c300dab4a97eb4a884e4fe0475762
Austin Bourdier <austinbourdier@gmail.com>
2014-08-01 15:32:15 -0700

    adding saferoute folder once again

--- SafeRoute/javascripts/init.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    SafeRoute.MasterController.initialize(SafeRoute.MapsController, SafeRoute.MapsView, SafeRoute.CrimesController, SafeRoute.CrimesModel, SafeRoute.RoutesController, SafeRoute.RoutesModel, SafeRoute.RoutesView, SafeRoute.UsersController, SafeRoute.UsersModel, SafeRoute.UsersView);
-});
-
-
-
